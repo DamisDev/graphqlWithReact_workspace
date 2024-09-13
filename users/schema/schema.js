3a3bee71e720f321117ec9e80f452016be613ad0
Giuseppe Damis <giuseppe.damis@digiservice-solutions.it>
2024-09-13 14:38:28 +0200

    added description for root query

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -16,15 +16,22 @@ const UserType = new GraphQLObjectType({
     }
 });
 
-// ROOT QUERY:
+
+/**
+ * In questa sezione abbiamo iniziato a implementare la nostra ROOT QUERY,
+ * che viene utilizzata per consentire a GraphQL di entrare nel grafo dei dati della nostra applicazione.
+ * La parte più importante di questa query principale (ROOT QUERY) è la funzione RESOLVE,
+ * che viene utilizzata per restituire un pezzo effettivo di dati dal nostro database,
+ * dal nostro archivio di dati o da qualsiasi cosa contenga i nostri dati.
+ * 
+ */
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         user: {
            type: UserType,
            args: { id: {type: GraphQLString}},
-           resolve(parentValue, args) {
-            
+           resolve(parentValue, args) {            
            }
         }
     }
