2d6fcb8dab6974e9a536368fcc61ecb4cc2fb635
Giuseppe Damis <giuseppe.damis@digiservice-solutions.it>
2024-09-16 13:16:54 +0200

    added resolve in a schema.js and added schema into a server.js

--- users/server.js
+++ users/server.js
@@ -1,11 +1,12 @@
 const expressGraphQL = require("express-graphql").graphqlHTTP;
 const express = require("express");
-
+const schema = require("./schema/schema");
 const app = express();
 
 app.use(
   "/graphql",
   expressGraphQL({
+    schema,
     graphiql: true,
   })
 );
